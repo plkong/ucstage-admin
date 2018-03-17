0a3d2985a5a94a9cb1386193deb928460c637f4e
plkong <379338796@QQ.COM>
2018-03-17 13:22:16 +0800

    add sql/ucstage.sql

---
+++ src/main/resources/sql/ucstage.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.7.19 : Database - ucstagedb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ucstagedb` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `ucstagedb`;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(125) COLLATE utf8_bin NOT NULL,
+  `pId` int(11) NOT NULL,
+  `remark` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `categoryId` int(11) NOT NULL,
+  `title` varchar(125) COLLATE utf8_bin DEFAULT NULL,
+  `color` varchar(125) COLLATE utf8_bin DEFAULT NULL,
+  `size` varchar(125) COLLATE utf8_bin DEFAULT NULL,
+  `stars` int(11) DEFAULT NULL,
+  `detail` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `thumbnail` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `images` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_categoryId` (`categoryId`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
